982808508a9ead99ea86fbba1f741a3af035b149
Johnathan Van Why <jrvanwhy@gmail.com>
2015-01-21 19:41:31 -0800

    Newer, faster (and simpler... mostly) quaternion class. There's some duplication between the multiply and rotate functions, but oh well.

--- atrias-robot/Quat.m
+++ atrias-robot/Quat.m
@@ -1,5 +1,5 @@
-% Time: 142.518
-classdef Quat < handle
+% Time: 131.585
+classdef Quat
 	methods
 		% Basic constructor. Can construct by giving the 4 values or by giving a rotation
 		% vector (which will be made into a rotation quaternion)
@@ -7,55 +7,47 @@ classdef Quat < handle
 			switch numel(vec)
 				case 3
 					angle = norm(vec);
-					vals = [cos(angle/2); sin(angle/2) * vec(:) / angle];
+					this.vals = [cos(angle/2); sin(angle/2) * vec(:) / angle];
 
 				case 4
-					vals = vec;
+					this.vals = vec(:);
 
 				otherwise
 					error(['Input vector size ' num2str(numel(vec)) ' is invalid. Must be 3 or 4'])
 			end
-
-			if all(size(vals) ~= [2 2])
-				this.mat = [1 0; 0 1] * vals(1) + [i 0; 0 -i] * vals(2) + [0 1; -1 0] * vals(3) + [0 i; i 0] * vals(4);
-			else
-				this.mat = vals;
-			end
 		end
 
 		% Quaternion conjugation function
-		function C = conj(this)
-			C = Quat(this.mat');
+		function this = conj(this)
+			this.vals = this.vals .* [1; -1; -1; -1];
 		end
 
 		% Quaternion multiplication function
-		function C = mtimes(A, B)
-			C = Quat(A.mat * B.mat);
+		function A = mtimes(A, B)
+			A.vals = (A.vals([1 2 3 4; 2 1 4 3; 3 4 1 2; 4 3 2 1]) .* [1 -1 -1 -1; 1 1 -1 1; 1 1 1 -1; 1 -1 1 1]) * B.vals;
 		end
 
 		% Function to perform a vector rotation by the inverse of the rotation represented by this quaternion.
 		function vec = invRot(this, vec)
-			vecmat  = [i 0; 0 -i] * vec(1) + [0 1; -1 0] * vec(2) + [0 i; i 0] * vec(3);
-			outQuat = this.mat' * vecmat * this.mat;
-			vec     = [imag(outQuat(1)); real(outQuat(3)); imag(outQuat(3))];
+			vec = (this.vals([2 3 4; 1 4 3; 4 1 2; 3 2 1]) .* [1 1 1; 1 1 -1; -1 1 1; 1 -1 1]) * vec;
+			vec = (vec([2 1 4 3; 3 4 1 2; 4 3 2 1]) .* [1 1 -1 1; 1 1 1 -1; 1 -1 1 1]) * this.vals;
 		end
 
 		% Function to rotate the given vector by the rotation represented
 		% by this quaternion
 		function vec = rot(this, vec)
-			vecmat  = [i 0; 0 -i] * vec(1) + [0 1; -1 0] * vec(2) + [0 i; i 0] * vec(3);
-			outQuat = this.mat * vecmat * this.mat';
-			vec     = [imag(outQuat(1)); real(outQuat(3)); imag(outQuat(3))];
+			vec = (this.vals([2 3 4; 1 4 3; 4 1 2; 3 2 1]) .* [-1 -1 -1; 1 -1 1; 1 1 -1; -1 1 1]) * vec;
+			vec = (vec([2 1 4 3; 3 4 1 2; 4 3 2 1]) .* [1 -1 1 -1; 1 -1 -1 1; 1 1 -1 -1]) * this.vals;
 		end
 
 		% Grab this quaternion as a vector of 4 real values
 		function vals = getVals(this)
-			vals = [real(this.mat(1)); imag(this.mat(1)); real(this.mat(3)); imag(this.mat(3))];
+			vals = this.vals;
 		end
 	end
 
 	properties
 		% The numeric values for this Quaternion
-		mat
+		vals
 	end
 end
