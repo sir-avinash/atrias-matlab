9c63d32941179de443848604c5d06f76bdbf85a1
Johnathan Van Why <jrvanwhy@gmail.com>
2015-02-07 11:25:23 -0800

    Vectorized checkMotion for the IMU alignment script.

--- atrias-robot/+imu/checkMotion.m
+++ atrias-robot/+imu/checkMotion.m
@@ -1,5 +1,5 @@
 % This checks if our motion is within the alignment tolerances.
 
 function is_moving = checkMotion(params, gyros, accels)
-	is_moving = (norm(gyros) >= params.align_gyro_tol || abs(norm(accels) - 1) >= params.align_accel_tol);
+	is_moving = (sum(gyros.^2) >= params.align_gyro_tol^2 | abs(sqrt(sum(accels.^2)) - 1) >= params.align_accel_tol);
 end
