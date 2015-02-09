f4b1ddff32c56e7613d05767b2f0080b1d51992d
Mikhail Jones <mikhailjones+github@gmail.com>
2015-02-09 14:29:24 -0800

    First commit of the simple 3D standing and walking gait extended from the current 2D gait. Only the simulation models included in this push.

---
+++ controllers/osu/simple-walk-3d/controller.m
@@ -0,0 +1,309 @@
+function [eStop, u, userOut] = controller(q, dq, userIn)
+%CONTROLLER Simple ATRIAS walking controller.
+%
+% Description:
+%   This controller unifies a number of fundamental concepts learned
+%   through observation of the SLIP model and past failed experiments.
+%   The controller is heuristically built up in layers and includes a few
+%   key concepts:
+%     * Passive stance leg behavior (zero hip torque, only axial forces)
+%     * Stance leg push off during second half of stance to inject energy
+%     * Slave swing leg trajectory to stance leg (with very loose gains)
+%     * Ground speed matching and fixed stride length before impact
+%     * Both legs stabilize torso but with a weight based on leg force
+%     * Leg relabeling occurs when swing leg force exceed stance leg force
+%
+% Notes:
+%   * Anything under 0.75 uses dynamic standing controller
+%   * Anything above 0.75 uses walk controller
+%   * If going from stand to walk, (same direction) do one step transistion
+%   * If going from stand to walk, (other direction) first ramp speed to
+%   zero then do one step transistion
+%   * If going from walk to stand, slow down, switch to stand
+%   * If switching direction of walk, first slow down then switch to stand,
+%   come to stop, then take single step in new direction
+%
+% Plan:
+%   * Verify walk works over obstacles and can reliable control speed
+%   * Verify stand can stand in place and stagger around under 0.75
+%   * Confirm simple stagger stand is best way to do things, being closer
+%   to walk controller would be better
+%   * Get fast walk to slow down and transition to stand smoothly.
+%   * Get stand to walk to work smoothly
+%   * Then think about othercase (velocity change rate limit when there is
+%   a sign flip
+%
+% Copyright 2015 Mikhail S. Jones
+
+  %% INITIALIZE =========================================================
+
+  % Set default emergency stop to false
+  eStop = false;
+
+  % Parse user input arguments into convient local variables
+  state = userIn(1); % Main controller state
+  u_lim = clamp(userIn(2), 0, 600); % Motor torque limit (N*m)
+  kp_leg = clamp(userIn(3), 0, 5000); % Leg motor proportional gain (N*m/rad)
+  kd_leg = clamp(userIn(4), 0, 500); % Leg motor differential gain (N*m*s/rad)
+  kp_hip = clamp(userIn(5), 0, 2000); % Hip motor proportional gain (N*m/rad)
+  kd_hip = clamp(userIn(6), 0, 200); % Hip motor differential gain (N*m*s/rad)
+  v_cmd = clamp(userIn(7), -1.5, 1.5); % Velocity (m/s)
+  l_ret = clamp(userIn(8), 0, 0.25); % Leg retraction (m)
+
+  % Gait parameters
+  ks_leg = 2950; % Leg rotational spring constant (N*m/rad)
+  l0 = 0.9; % Nominal leg length (m)
+  q0_torso = 0; % Target torso pitch (rad)
+  s_leg = 0.5; % Scale leg actuator gains for swing phase
+  s_torso = 0.5; % Scale leg actuator gains for torso stabilization
+  threshold = 50; % Spring torque threshold for scaling and switching (N*m)
+
+  % % Simulation time and velocity profile
+  % persistent T; if isempty(T); T = 0; else T = T + 0.001; end % if
+  % v_cmd = 1.2*(3 < T & T < 10) - 1.2*(13 < T & T < 20);
+
+  % Persistent variable to keep track of current stance leg
+  persistent stanceLeg; if isempty(stanceLeg); stanceLeg = 1; end % if
+
+  % Persistent variable to keep track of extra swing leg clearence
+  persistent l_clr; if isempty(l_clr); l_clr = 0; end % if
+
+  % Persistent variable to keep track of states at last switch
+  persistent x_st_e; if isempty(x_st_e); x_st_e = 0; end % if
+  persistent x_sw_e; if isempty(x_sw_e); x_sw_e = 0; end % if
+
+  % Persistent variable to keep track controller type
+  persistent v_tgt; if isempty(v_tgt); v_tgt = v_cmd; end % if
+  persistent isStand; if isempty(isStand); isStand = true; end % if
+
+  % Persistent variable to keep track of time since last switch
+  persistent t; if isempty(t); t = 0; else t = t + 0.001; end % if
+
+  persistent d; if isempty(d); d = 0; end % if
+
+  % Setup stance and swing indexes
+  if stanceLeg == 1 % Left
+    leg_m = [8 6 4 2]; leg_l = [7 5 3 1]; leg_u = [5 4 2 1];
+    hip_m = [10 9]; hip_u = [6 3];
+  else % Right
+    leg_m = [4 2 8 6]; leg_l = [3 1 7 5]; leg_u = [2 1 5 4];
+    hip_m = [9 10]; hip_u = [3 6];
+  end % if
+
+  % Initialize input vector to zeros
+  u = zeros(6,1);
+
+  % Initialize user output vector
+  userOut = zeros(1,1);
+
+  %% MAIN CONTROLLER ====================================================
+
+  switch state
+  case 0 % STAND --------------------------------------------------------
+    % Target leg actuator positions
+    q0_leg = pi + [-1; 1; -1; 1]*acos(l0);
+
+    % Target leg actuator velocities
+    dq0_leg = zeros(4,1);
+
+    % Leg actuator torques from PD controller
+    u(leg_u) = (q0_leg - q(leg_m))*kp_leg + (dq0_leg - dq(leg_m))*kd_leg;
+
+    % Target hip actuator positions
+    q0_hip = [-0.08; 0.08];
+
+    % Target hip actuator velocities
+    dq0_hip = zeros(2,1);
+
+    % Hip actuator torques from PD controller
+    u(hip_u) = (q0_hip - q(hip_m))*kp_hip + (dq0_hip - dq(hip_m))*kd_hip;
+
+  case 1 % WALK ---------------------------------------------------------
+    % Cartesian position of toes relative to hip in world frame
+    x_st = sum(sin(q(13) + q(leg_l(1:2)))/2);
+    y_st = sum(cos(q(13) + q(leg_l(1:2)))/2);
+    x_sw = sum(sin(q(13) + q(leg_l(3:4)))/2);
+    y_sw = sum(cos(q(13) + q(leg_l(3:4)))/2);
+
+    % Compute COM states
+    l_l = cos((q(leg_l(2)) - q(leg_l(1)))/2);
+    l_h = 0.18*stanceLeg;
+    l_t = 0.3434;
+    dx = l_l*mean(dq(13) + dq(leg_l(1:2))) + l_t*cos(q(13))*dq(13);
+    dy = (l_l*cos(q(hip_m(1)) + q(11)) - l_h*sin(q(hip_m(1)) + q(11)))*(dq(hip_m(1)) + dq(11)) + l_t*cos(q(11))*dq(11);
+
+    % Stance leg push-off is proportional to desired speed and error
+    l_ext = clamp(...
+      1/30*abs(v_tgt) + ...
+      1/20*sign(v_tgt)*(v_tgt - dx), ...
+      0, 0.05)*(sign(v_tgt) == sign(dx));
+
+    % Tune parameters for desired speed
+    if isStand
+      % Step length is proportional to current velocity and error
+      l_step = clamp(...
+        0.2*dx - ...
+        clamp(0.4*(v_tgt - dx), -0.1, 0.1) - ...
+        x_st, -0.5, 0.5);
+
+      % Set leg swing trigger point
+      trig = 0.8;
+
+      % Define a time variant parameter
+      s = t/0.5;
+    else
+      % Step length is constant and in direction of target velocity
+      l_step = sign(v_tgt)*0.4;
+
+      % Set leg swing trigger point
+      trig = 0.6;
+
+      % Define time invariant parameter based on hip position
+      s = (x_st_e - x_st)/(x_st_e + l_step/2);
+    end % if
+
+    % Swing leg retraction policy (immediately retract then extend once
+    % past defined trigger point)
+    l_sw = l0 - (l_ret + l_clr)*(s < trig);
+
+    % Swing leg swing policy (use cubic spline to interpolate target
+    % ground projection point of the toe and find the corresponding leg
+    % angle given a desired length)
+    d_sw = cubic(0, 0.7, x_sw_e - x_st_e, l_step, 0, 0, s, 1);
+    r_sw = pi/2 + acos((x_st + d_sw)/l_sw) - q(13);
+
+    % Target swing leg actuator positions
+    q_sw = r_sw + [-1; 1]*acos(l_sw);
+
+    % Target swing leg actuator velocities
+    dq_sw = zeros(2,1);
+
+    % Swing leg actuator torques from PD controller
+    u(leg_u(3:4)) = (q_sw - q(leg_m(3:4)))*kp_leg*s_leg + (dq_sw - dq(leg_m(3:4)))*kd_leg*s_leg;
+
+    % Stance leg push off policy (extend leg after mid stance linearly)
+    l_st = l0 + l_ext*clamp(2*s - 1, 0, 1);
+
+    % Stance leg step down policy (retract leg if drop is detected)
+    l_st = l_st + clamp(y_sw - y_st, -l_ret, 0);
+
+    % Target stance leg actuator positions
+    q_st = mean(q(leg_l(1:2))) + [-1; 1]*acos(l_st);
+
+    % Target stance leg actuator velocities
+    dq_st = [1; 1]*mean(dq(leg_l(1:2)));
+
+    % Stance leg actuator torques from PD controller
+    u(leg_u(1:2)) = (q_st - q(leg_m(1:2)))*kp_leg + (dq_st - dq(leg_m(1:2)))*kd_leg;
+
+    % Scaling terms for torso stabilization and state switching is
+    % based on absolute mean torque in springs scaled and clamped
+    % between 0 and 1
+    s_st = clamp(ks_leg/threshold*mean(abs(q(leg_m(1:2)) - q(leg_l(1:2)))), 0, 1);
+    s_sw = clamp(ks_leg/threshold*mean(abs(q(leg_m(3:4)) - q(leg_l(3:4)))), 0, 1);
+
+    % Add additional torque commands to leg actuators to stabilize
+    % scaled based on the "force" felt in the leg
+    u(leg_u(1:2)) = u(leg_u(1:2)) + ...
+      s_st*s_torso*((q(13) - q0_torso)*kp_leg + dq(13)*kd_leg);
+    u(leg_u(3:4)) = u(leg_u(3:4)) + ...
+      s_sw*s_torso*((q(13) - q0_torso)*kp_leg + dq(13)*kd_leg);
+
+    % Stop lateral adjusment after target TD
+    if s < 1
+      d = -0.08*stanceLeg - 0.3*dy;
+      d = clamp(d, -0.5, 0.5); % Prevent asin errors
+    end % if
+
+    % Inverse kinematics
+    L = sqrt(l_l^2 + l_h^2);
+    q1 = asin(d/L);
+    q2 = asin(-l_h/L);
+    q_h = q1 - q2 - q(11);
+    q_h = clamp(q_h, -0.15*stanceLeg, 0.3*stanceLeg);
+    dq_h = 0;
+
+    % Swing leg PD controller
+    u(hip_u(2)) = u(hip_u(2)) + ...
+      (q_h - q(hip_m(2)))*kp_hip + (dq_h - dq(hip_m(2)))*kd_hip;
+
+    % Torso stabilization weighted PD controller
+    u(hip_u) = u(hip_u) + ...
+      [s_st; s_sw].*(q(11)*kp_hip + dq(11)*kd_hip);
+
+    % Detect when swing leg force exceeds stance leg force
+    if (s_sw > s_st && t > 0.2) || (isStand && s >= 1)
+      % Switch stance legs
+      stanceLeg = -stanceLeg;
+
+      % Estimate extra required swing leg clearence in case of step
+      l_clr = ~isStand*clamp(abs(y_sw - y_st), 0, 0.15); % TODO: check if isStand is needed
+
+      % Reset time since last switch
+      t = 0;
+
+      % Exit conditions
+      x_st_e = x_sw;
+      x_sw_e = x_st;
+
+      % Check standing speed or direction flip of commanded velocity
+      if abs(v_cmd) < 0.75 || sign(v_cmd) ~= sign(dx)
+        % Walk slower
+        if abs(dx) > 1 && ~isStand
+          v_tgt = sign(dx)*0.75;
+        % Stand
+        else
+          v_tgt = sign(v_cmd)*0.25;
+          isStand = true;
+        end % if
+      else
+        % Walk normal
+        v_tgt = v_cmd;
+        isStand = false;
+      end % if
+    end % if
+
+    % Error catch for falling backwards
+    if abs(dx) < 0.01; isStand = true; end % if
+
+    % User outputs
+    userOut = q_h;
+
+  otherwise % RELAX -----------------------------------------------------
+    % Leg actuator torques computed to behave like virtual dampers
+    u(leg_u) = (0 - dq(leg_m))*kd_leg;
+    u(hip_u) = (0 - dq(hip_m))*kd_hip;
+  end % switch
+
+  % Limit absolute torque commands
+u = clamp(u, -u_lim, u_lim);
+end % controller
+
+%% LOCAL FUNCTIONS ========================================================
+
+function b = clamp(a, lim1, lim2)
+%CLAMP Clamp value between two bounds.
+
+  % Find which limit is min and max
+  a_min = min(lim1, lim2);
+  a_max = max(lim1, lim2);
+
+  % Clamp value between limits
+  b = max(min(a, a_max), a_min);
+end % clamp
+
+function [y, dy] = cubic(x1, x2, y1, y2, dy1, dy2, x, dx)
+%CUBIC Cubic interpolation between values.
+
+  % Limit range since curve fit is only valid within range
+  x = clamp(x, x1, x2);
+
+  % Interpolate
+  a0 = 2*(y1 - y2) + (dy1 + dy2)*(x2 - x1);
+  a1 = y2 - y1 - dy1*(x2 - x1) - a0;
+  a2 = dy1*(x2 - x1);
+  a3 = y1;
+  s = (x - x1)/(x2 - x1);
+  y = a0*s^3 + a1*s^2 + a2*s + a3;
+  dy = dx*(-3*a0*(x - x1)^2/(x1 - x2)^3 + 2*a1*(x - x1)/(x1 - x2)^2 - a2/(x1 - x2));
+end % cubic
---
+++ controllers/osu/simple-walk-3d/mechanical_params.m
@@ -0,0 +1,210 @@
+%MECHANICAL_PARAMS Mechanical properties of ATRIAS for simulation.
+%
+% Notes:
+%   - All quantities are in SI Units
+
+%% ENVIRONMENTAL ==========================================================
+g = 9.81;
+
+%% SENSOR NOISE ===========================================================
+sensor_noise_variance = (2e-4)^2*ones(13,1);
+
+%% MOTORS =================================================================
+
+% Saggital
+k_leg_spring = 2950;
+b_leg_spring = 0.075*sqrt(k_leg_spring*0.5*.25^2);
+N = 50;
+mu_motor = 0.85;
+b_rotor = 0*60/(2*pi)*9.725e-5;
+i_rotor = 1.5e-3;
+i_rotor_moments = [0 0 i_rotor];
+i_motor_reflected = i_rotor * N^2;
+r_rotor = 0.001;
+m_rotor = 1e-5;
+r_virtual_gear = N*r_rotor;
+m_virtual_gear = 1e-5;
+i_virtual_gear = [0 0 1e-5];
+m_output_gear = 1e-5;
+i_output_gear = [0 0 1e-5];
+m_gearing = m_rotor+m_virtual_gear+m_output_gear;
+Kt_leg = 0.121;
+Kb_leg = 0.121;
+R_leg = 0.052;
+L_leg = 0.102e-3;
+max_voltage_leg = 50;
+max_current_leg = 200;
+LEG_MTR_MAX_TORQUE = max_current_leg * Kt_leg * N;
+
+% Lateral
+r_hip_gearhead = 0.009525;
+r_hip_shaft = 0.542925;
+N_h = r_hip_shaft / r_hip_gearhead;
+mu_hmotor = 0.90;
+b_hmotor = 0;
+i_hrotor_moments = [0 0 1.5e-3];
+max_current_hip = 60;
+Kt_hip = 0.184;
+HIP_MTR_MAX_TORQUE = max_current_hip*Kt_hip*N_h;
+
+% Friction (Specified after the gear reduction)
+breakaway_friction = [25 25 25 25];
+coulomb_friction = [20 20 20 20];
+viscous_friction = [0.001 0.001 0.001 0.001];
+friction_linear_zone = [0.5 0.5 0.5 0.5].*pi/180; % velocity zone before full stiction is applied
+
+%% LEG GEOMETRY ===========================================================
+l_seg = 0.5;
+l_rad = 0.03;
+
+% Thigh
+com_thigh = [0.0160 -0.1822 0];
+m_thigh = 0.69;
+i_thigh_moments = [0.02 0 0.02];
+i_thigh_products = [0 0 0];
+
+% Lower Leg
+lowerleg_12 = 0.40;
+com_lowerleg = [0 -0.1513 0];
+m_lowerleg = 0.4438;
+i_lowerleg_moments = [0.01 0 0.01];
+i_lowerleg_products = [0 0 0];
+l_lowerleg = 0.49;
+
+% Foot
+d_foot_point_front = [0.06350 -0.02515 0];
+d_foot_point_back = [-0.06350 -0.02515 0];
+m_foot = 0.0857;
+com_foot = [0 -0.00959 0];
+i_foot_moments = [0.00001 0.00006 0.00006];
+i_foot_products = [0 0 0];
+ankle_stiffness = 1/2*pi;
+ankle_equilibrium = 25*pi/180;
+
+% Four Bar Link
+com_fblink = [0 -0.1137 0];
+m_fblink = 0.46;
+i_fblink_moments = [0.01 0 0.01];
+i_fblink_products = [0 0 0];
+
+% Shin
+shin_12 = 0.40;
+com_shin = [0.0215 -0.1503 0];
+m_shin = 0.75;
+i_shin_moments = [0.02 0 0.02];
+i_shin_products = [0 0 0];
+
+% Mechanical Limits Model
+k_phi = 5000; % N/m
+v_phi_max = 1/60; % rad/s
+delta_phi_min = 25.5*pi/180;
+delta_phi_max = 160*pi/180;
+phi_shin_max = 3*pi/2 + 17.5*pi/180;
+phi_shin_min = pi - 45*pi/180;
+phi_thigh_max = pi + 45*pi/180;
+phi_thigh_min = pi/2 - 17.5*pi/180;
+phi_roll_max = pi + 16*pi/180;
+phi_roll_min = pi - 9*pi/180;
+phi_max_collision = -5*pi/180;
+
+% Hip Shaft
+leg_offset = 0.1831;
+hshaft_12 = [0 0 leg_offset];
+com_hshaft = [0 0.029 leg_offset-0.0149];
+m_hshaft = 18; % includes spring plates
+i_hshaft_moments = [0.29 0.10 0.27];
+i_hshaft_products = [0 0 0.1];
+
+%% BOOM ===================================================================
+pitch_mount_angle = 7.2824 * pi/180;
+
+% Boom tube
+l_boom = 1.8161;
+boom_12 = [0 0 -l_boom];
+com_boom = [0 0 -1.33];
+m_boom = 4.49;
+i_boom_moments = [3.9 3.9 0];
+i_boom_products = [0 0 0];
+
+% Yaw shaft
+yshaft_12 = [0 0.9906 0];
+com_yshaft = [0 1 -0.15];
+m_yshaft = 12.89;
+i_yshaft_moments = [8 3.3 4.9];
+i_yshaft_products = [-3 0 0];
+
+% Boom base
+base_12 = [0 0 0];
+
+% Counter weight
+counterweight_mass = 0;
+
+%% TORSO ==================================================================
+boom_mount_to_hip = 0.318;
+boom_mount_to_center = 0.2225;
+boom_mount_to_center_diagonal = boom_mount_to_center / cos(pitch_mount_angle);
+torso_12 = [0 -boom_mount_to_hip -boom_mount_to_center];
+com_torso = [0 0.05 -0.2277];%[0 0.05 -0.1577];
+m_torso = 17.5;
+i_torso_moments = [1.5 1.5 2.2];
+i_torso_products = [0 0 0];
+winch_hook_offset = [0 0.375 -0.2225];
+d_hip_com_to_torso_com = boom_mount_to_hip - com_hshaft(2) + com_torso(2);
+d_pelvis_to_torso = boom_mount_to_hip + com_torso(2);
+d_pelvis_to_IMU = [4.720 18.770 6.327]*2.54/100;
+
+%% OTHER ==================================================================
+
+% Total massess and inertias
+num_legs = 2;
+m_total = num_legs*(m_hshaft + m_thigh + m_lowerleg + m_shin + m_fblink + 2*m_gearing) + m_torso;
+m_leg = m_thigh + m_lowerleg + m_shin + m_fblink;
+m_hip = m_hshaft + m_leg;
+i_motor = i_motor_reflected + m_leg/2*l_seg^2;
+i_leg = m_leg/2*l_seg^2;
+d_com = (d_hip_com_to_torso_com*m_torso)/(m_torso + num_legs*m_hshaft);
+boom_mount_to_com = boom_mount_to_hip - d_com;
+i_robot = i_torso_moments(3) + m_torso*(d_hip_com_to_torso_com - d_com)^2 + num_legs*m_hshaft*d_com^2 + num_legs*i_hshaft_moments(1);
+
+%% INITIAL CONDITIONS =====================================================
+initial_r_leg_length = 0.9;
+initial_r_leg_angle =  pi;% + 0.2;
+thigh_initial_r = initial_r_leg_angle - acos(initial_r_leg_length);
+shin_initial_r = initial_r_leg_angle + acos(initial_r_leg_length);
+initial_l_leg_length = 0.9;
+initial_l_leg_angle = pi;% - 0.2;
+thigh_initial_l = initial_l_leg_angle - acos(initial_l_leg_length);
+shin_initial_l = initial_l_leg_angle + acos(initial_l_leg_length);
+v_thigh_initial_r =  0;
+v_shin_initial_r =  0;
+
+initial_com_height = d_com + initial_r_leg_length + 0.02;
+initial_torso_yaw = 0*pi/180;
+initial_torso_pitch = 0*pi/180;
+initial_torso_roll = 0*pi/180;
+initial_pitch_joint_height = initial_com_height + boom_mount_to_com;
+initial_hip_roll = -0.08;
+initial_dx = 0;
+
+%% CONTACT MODEL ==========================================================
+
+% Vertical ground interaction stiffness
+k_gy = m_total*g/0.005; % [N/m]
+
+% Maximum vertical ground relaxation speed
+v_gy_max = 0.03; %[m/s]
+
+% Horizontal ground interaction stiffness
+k_gx = m_total*g/0.005; % [N/m]
+
+% Maximum horizontal ground relaxation speed
+v_gx_max = 0.03; %[m/s]
+
+% Stiction coefficient
+mu_stick = 0.9;
+
+% Sliding coefficient
+mu_slide = 0.8;
+
+% Slip-stic transition velocity
+vLimit = 0.01; %[m/s]
